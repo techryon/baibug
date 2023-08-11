40655f51f16e5669cee96e8143a6da79912ea8f4
Michael <michael.allerby@dovetailstudios.com>
2023-08-11 17:40:02 +1200

    Add results coming back

--- sidebar_provider.js
+++ sidebar_provider.js
@@ -57,62 +57,88 @@ class SidebarProvider {
           <!DOCTYPE html>
             <html lang="en">
             <head>
-            <script>
-              function callOpenAi() {
-                var myHeaders = new Headers();
-                var token = "YOUR_TOKEN_HERE";
-                myHeaders.append("Content-Type", "application/json");
-                myHeaders.append("Accept", "application/json");
-                myHeaders.append("Authorization", "Bearer " + token);
+              <style>
+                .bug_list > p {
+                  font-weight: 600;
+                  text-overflow: ellipsis;
+                  overflow: hidden;
+                  white-space: nowrap;
+                  cursor: pointer;
+                  text-decoration: underline;
+                }
+              </style>
 
-                const raw = JSON.stringify(
-                  {
-                    "model": "gpt-3.5-turbo",
-                    "messages": [
-                      {
-                        "role": "user",
-                        "content": "Foobar"
-                      },
-                      {
-                        "role": "user",
-                        "content": "Foobar"
-                      },
-                      {
-                        "role": "user",
-                        "content": "Foobar"
-                      },    
-                    ],
-                    "temperature": 0,
-                    "max_tokens": 7
-                  }
-                )
+              <script>
+                function callOpenAi() {
+                  var text = event.target.innerText;
+                  var code = event.target.getAttribute("data-code");
 
-                var requestOptions = {
-                  method: 'POST',
-                  headers: myHeaders,
-                  body: raw,
-                  redirect: 'manual'
-                };
-                
-                fetch("https://api.openai.com/v1/chat/completions", requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
-              }
-            </script>
+                  var myHeaders = new Headers();
+                  var token = "OPENAI_API_KEY";
+                  myHeaders.append("Content-Type", "application/json");
+                  myHeaders.append("Accept", "application/json");
+                  myHeaders.append("Authorization", "Bearer " + token);
+
+                  const raw = JSON.stringify(
+                    {
+                      "model": "gpt-3.5-turbo",
+                      "messages": [
+                        {
+                          "role": "user",
+                          "content": text
+                        },
+                        {
+                          "role": "user",
+                          "content": code
+                        },
+                        {
+                          "role": "user",
+                          "content": "look at the stack trace referenced in this message and respond in valid JSON where the first key is 'human_readable_error' and value is a human readable explanation of the error, and the second key is 'code_suggestion' and the value is valid Javascript that would fix the error without textual explanation, and a third key that is 'line_number' with the line number where the code suggestion should be"
+                        },    
+                      ],
+                      "temperature": 0.5,
+                      "top_p": 1,
+                      "n": 1,
+                      "stream": false,
+                      "max_tokens": 350,
+                      "presence_penalty": 0,
+                      "frequency_penalty": 0
+                    }
+                  )
+
+                  var requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: raw,
+                    redirect: 'manual'
+                  };
+                  
+                  fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+                  .then(response => response.text())
+                  .then(result => {
+                    var json = JSON.parse(result);
+                    var results = document.getElementById("results");
+                    var resultsP = results.getElementsByTagName("p")[0];
+                    var resultsCode = results.getElementsByTagName("code")[0];
+                    var code = JSON.parse(json.choices[0].message.content);
+                    resultsP.innerHTML = code.human_readable_error;
+                    resultsCode.innerHTML = code.code_suggestion;
+                  })
+                  .catch(error => console.log('error', error));
+                }
+              </script>
             </head>
             <body>
                 <div id="app">
-                  <ul id="error_list">
-                    <li>
-                      <a class="test" onclick="callOpenAi()">
-                        Foo
-                      </a>
-                    </li>
-                  </ul>
-
+                  <div class="bug_list">
+                    <p onclick="callOpenAi()"
+                       data-code="import React, { useState } from 'react';function BugComponent(){const [count, setCount] = useState;  const handleIncrement = () => {    setCount(count + 1);  };  return (    <div>      <h1>Buggy React Component</h1><p>Current count: {conut}</p><button onClick={handleIncrement}>Increment</button></div>);} export default BugComponent;">
+                      Uncaught ReferenceError: setcount is not defined at handleIncrement (App.jsx:9:5) at HTMLUnknownElement.callCallback2 (react-dom.development.js:4164:14) at Object.invokeGuardedCallbackDev (react-dom.development.js:4213:16) at invokeGuardedCallback (react-dom.development.js:4277:31) at invokeGuardedCallbackAndCatchFirstError (react-dom.development.js:4291:25) at executeDispatch (react-dom.development.js:9041:3) at processDispatchQueueItemsInOrder (react-dom.development.js:9073:7) at processDispatchQueue (react-dom.development.js:9086:5) at dispatchEventsForPlugins (react-dom.development.js:9097:3) at react-dom.development.js:9288:12
+                    </p>
                   <div id="results">
-                    // Put results here
+                    <h2>Results</h2>
+                    <p></p>
+                    <code></code>
                   </div>
                 </div>
                 <script>
